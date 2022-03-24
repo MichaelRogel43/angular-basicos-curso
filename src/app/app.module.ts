9fff173f0709fae2d3fed77d9d6a9ef9d7377e5f
Michael Rogel <michaelrogel43@gmail.com>
2022-03-24 12:47:13 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador/contador.module';
+
+
+import { HeroesModule } from './Heroes/Heroes/Heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule, 
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
